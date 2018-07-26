7aff6458f211f6783932ac18efb30a63eda311e6
Alexey Vinnik <AlexeyVinnik@ocsico.com>
2018-07-26 15:25:53 +0300

    Updated build configurations. Implemented circled avatars.

--- app/src/main/java/test/com/test/ui/adapters/UserListAdapter.java
+++ app/src/main/java/test/com/test/ui/adapters/UserListAdapter.java
@@ -8,11 +8,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.ImageLoader;
-import com.android.volley.toolbox.NetworkImageView;
 
 import java.util.ArrayList;
 
+import de.hdodenhof.circleimageview.CircleImageView;
 import test.com.test.R;
 import test.com.test.model.UserInfo;
 import test.com.test.net.NetAPI;
@@ -45,7 +46,21 @@ public class UserListAdapter extends RecyclerView.Adapter<UserListAdapter.ViewHo
     public void onBindViewHolder(final ViewHolder holder, final int position) {
         final UserInfo item = mDataSet.get(position);
         holder.mUserName.setText(item.owner.name + " " + item.owner.surname);
-        holder.mUserImage.setImageUrl(item.owner.foto, mImageLoader);
+
+        if (holder.mImageContainer != null) {
+            holder.mImageContainer.cancelRequest();
+        }
+        holder.mImageContainer = mImageLoader.get(item.owner.foto, new ImageLoader.ImageListener() {
+            @Override
+            public void onResponse(ImageLoader.ImageContainer response, boolean isImmediate) {
+                holder.mUserImage.setImageBitmap(response.getBitmap());
+            }
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //do nothing
+            }
+        });
 
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -72,13 +87,14 @@ public class UserListAdapter extends RecyclerView.Adapter<UserListAdapter.ViewHo
     static class ViewHolder extends RecyclerView.ViewHolder {
 
         private TextView mUserName;
-        private NetworkImageView mUserImage;
+        private CircleImageView mUserImage;
+        private ImageLoader.ImageContainer mImageContainer;
 
         ViewHolder(View itemView) {
             super(itemView);
 
             mUserName = (TextView) itemView.findViewById(R.id.user_name);
-            mUserImage = (NetworkImageView) itemView.findViewById(R.id.user_icon);
+            mUserImage = (CircleImageView) itemView.findViewById(R.id.user_icon);
         }
     }
 }
